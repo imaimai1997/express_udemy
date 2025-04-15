6024c7559b6dd24f7753cd474249fe78f0ec6ebc
imaimai <onakasuitanaapple@gmail.com>
2025-04-15 16:37:50 +0900

    try express

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const PORT = process.env.PORT;
-console.log(PORT);
+const PORT = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.end("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Application listening at ${PORT}`);
+});
