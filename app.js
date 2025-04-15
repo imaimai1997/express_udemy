a08188be55899c8985fe23b2a147357c001b1f4b
imaimai <onakasuitanaapple@gmail.com>
2025-04-15 19:02:56 +0900

    静的ファイルの出力

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 const express = require("express");
 const app = express();
 
+//Express setting
 app.set("view engine", "ejs");
+
+//Static resource rooting
+//publicにアクセスしたとき、現在のパスから相対パスであるpublicからファイルを探して返す。
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+//Dynamic resource rooting
 app.use("/", require("./routes/index.js"));
 
 app.listen(PORT, () => {
