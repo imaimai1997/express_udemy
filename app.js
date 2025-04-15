ff27b49350474f2864cc5139ac6246b8c7d5912c
imaimai <onakasuitanaapple@gmail.com>
2025-04-15 17:00:40 +0900

    ejs try

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.end("Hello World");
-});
+app.set("view engine", "ejs");
+app.use("/", require("./routes/index.js"));
 
 app.listen(PORT, () => {
   console.log(`Application listening at ${PORT}`);
