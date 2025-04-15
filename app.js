041823fc3a7f8fc5114bef0164ef501a6bc9a12a
imaimai <onakasuitanaapple@gmail.com>
2025-04-15 19:09:13 +0900

    favicon install

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const PORT = process.env.PORT || 3000;
 const path = require("path");
 const express = require("express");
+const favicon = require("serve-favicon");
 const app = express();
 
 //Express setting
 app.set("view engine", "ejs");
 
 //Static resource rooting
+app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 //publicにアクセスしたとき、現在のパスから相対パスであるpublicからファイルを探して返す。
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
