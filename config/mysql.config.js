0d38689a2539153d0e72a437f58dd56988d3586f
imaimai <onakasuitanaapple@gmail.com>
2025-04-23 20:50:48 +0900

    mysqlの設定

---
+++ config/mysql.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  HOST: process.env.MYSQL_HOST ,
+  PORT: process.env.MYSQL_PORT ,
+  USERNAME: process.env.MYSQL_USERNAME,
+  PASSWORD: process.env.MYSQL_PASSWORD ,
+  DATABASE: process.env.MYSQL_DATABASE ,
+};
