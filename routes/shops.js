375cd0e8540d5507bc0ddaba91433fa5573d534e
imaimai <onakasuitanaapple@gmail.com>
2025-04-26 17:57:32 +0900

    口コミデータ取得

--- routes/shops.js
+++ routes/shops.js
@@ -6,9 +6,11 @@ router.get("/:id", async (req, res, next) => {
 
   Promise.all([
     mySQLClient.executeQuery(await sql("SELECT_SHOP_DETAIL_BY_ID"), [id]),
+    mySQLClient.executeQuery(await sql("SELECT_SHOP_REVIEW_BY_SHOP_ID"), [id]),
   ])
     .then((results) => {
       var data = results[0][0];
+      data.reviews = results[1] || [];
       res.render("./shops/index.ejs", data);
     })
     .catch((err) => {
