2f9afde2b9bf8a4ea2f662e6096c140fd0434371
imaimai <onakasuitanaapple@gmail.com>
2025-05-01 12:56:22 +0900

    認証機能の実装

--- routes/account.js
+++ routes/account.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
+const {
+  authenticate,
+  authorize,
+  PRIVILEGE,
+} = require("../lib/security/accesscontrol.js");
 
 router.get("/login", (req, res, next) => {
-  res.render("./account/login.ejs");
+  res.render("./account/login.ejs", { message: req.flash("message") });
 });
+router.post("/login", authenticate());
 
 router.use("/reviews", require("./account.reviews.js"));
 
